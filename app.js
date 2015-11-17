6fcc74d904f1f27d33ef1db8d7ecb37c1bde389f
Timo Leinonen <twl@avaleo.net>
2015-11-17 10:32:48 +0100

    Chat tutorial

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ app.use(express.static('public'));
 app.use('/bower_components',express.static(__dirname + '/bower_components'));
 
 io.on('connection',function(socket){
-  console.log('Connected to socket');
-  socket.on('disconnect',function(){
-     console.log('Disconnect to socket');
-  })
+    console.log('Connected to socket');
+    socket.on('disconnect', function(){
+        console.log('Disconnect to socket');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(80,function(){
-  console.log('Example app listening at http://localhost:80');
+    console.log('Example app listening at http://localhost:80');
 });
