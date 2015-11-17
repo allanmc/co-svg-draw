c7aba116de721a1f3add2c7dd1b44fae4a21a7f2
Timo Leinonen <twl@avaleo.net>
2015-11-17 10:02:30 +0100

    Serving static files.

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 var express = require('express');
 var path = require('path');
+var io = require('socket.io');
 var app = express();
 
- /* serves main page */
- app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
- });
+ //app.get("/", function(req, res) {
+ //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+ //});
 
- /* serves all the static files */
- app.get(/^(.+)$/, function(req, res){
-     console.log('static file request : ' + req.params);
-     res.sendfile( __dirname + req.params[0]);
- });
+app.use(express.static('public'));
+app.use('/bower_components',express.static(__dirname + '/bower_components'));
 
 var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
+  io.listen(server);
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
