203790b9ad0d8d5d9e3523f6ec21e8e99fd6e7d6
Timo Leinonen <twl@avaleo.net>
2015-11-17 10:13:50 +0100

    Socket io script

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var path = require('path');
-var io = require('socket.io');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 app.use('/bower_components',express.static(__dirname + '/bower_components'));
 
-var server = app.listen(80, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  io.listen(server);
+io.on('connection',function(socket){
+  console.log('Connected to socket');
+  socket.on('disconnect',function(){
+     console.log('Disconnect to socket');
+  })
+});
 
-  console.log('Example app listening at http://%s:%s', host, port);
+http.listen(80,function(){
+  console.log('Example app listening at http://localhost:80');
 });
