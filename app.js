277490efec2e5976aa7488a47f1bfba2ccbf961c
Timo Leinonen <twl@avaleo.net>
2015-11-17 09:40:38 +0100

    Added web server on port 80. Added npm start script.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+ /* serves main page */
+ app.get("/", function(req, res) {
+    res.sendfile('public/index.html');
+ });
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){
+     console.log('static file request : ' + req.params);
+     res.sendfile( __dirname + req.params[0]);
+ });
+
+var server = app.listen(80, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
